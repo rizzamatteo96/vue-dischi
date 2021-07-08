7d8dd36a46f4aecd97d8c935c967cfad2e3a1bce
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-07-08 14:17:06 +0200

    init project

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <img src="../assets/img/logo.png" alt="logo spotify">
+  </header>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style lang="scss">
+  header{
+    background-color: #2e3a46;
+    height: 100px;
+    padding: 10px 20px;
+
+    img{
+      height: 80px;
+    }
+  }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    ciao
+  </div>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style lang="scss">
+  .container{
+    width: 80%;
+  }
+</style>
